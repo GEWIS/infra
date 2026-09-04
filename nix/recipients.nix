e223b0f7ded8f67beed91fa83482873d63700c69
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-09-04 11:46:55 +0200

    feat: add pcgewisc, d and update keys

--- nix/recipients.nix
+++ nix/recipients.nix
@@ -16,6 +16,14 @@
       key = "age1hde6y6ee9g0pnvgdluxjj6xq4plrsdq8f7mf4yv6xn465ucpw3hq47heg0";
       adminReadable = false;
     };
+    pcgewisc = {
+      key = "age1q4lkanpv39xp36w0symj2uulgpr924ghvl56j0rnnphcz7qxr32spk9lkc";
+      adminReadable = true;
+    };
+    pcgewisd = {
+      key = "age1xjny3erjq38f8p0yu29ag6dsgpk9y4wduhahqv3vf2uapa60gs3qxm3e06";
+      adminReadable = true;
+    };
 
     s3-01 = {
       key = "age1r9nz065avdtvrx2rqyq5lztf53dw8ds0uvkxkuw2zgvkqqx5e9js347zqu";
